2ca2b0938a174b74584bb53782c9dbc528deb944
Hakuchi <71324797+hakuchi-1864@users.noreply.github.com>
2022-10-21 17:37:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# It is to time function and return average time
+
+def time_fn(fn, *args, rep=4, **kwargs):
+  start = time.perf_counter()
+  for i in range(rep):
+    fn(*args, **kwargs)
+  end = time.perf_counter()
+  return(end - start) / rep
+# Create some functions to time them
+
+def compute_fn_1(n, *, start=0, end):
+  results = []
+  for i in range(start, end):
+    results.append(i**3)
+  return results
+
+def compute_fn_2(n, *, start = 1, end):
+   return [n**i for i in range(start, end)] 
+
+def compute_fn_2(n, *, start = 1, end):
+  
+    
+
+   
+   
+   
